73a420b9adbc95f4c58bc2caa0046d4f59a31116
dakotamum <dakotamum@gmail.com>
2019-04-22 13:04:18 -0600

    Reads in data and prints out time and velocities

---
+++ readTheDatas.py
@@ -0,0 +1,39 @@
+import csv
+
+#readFile = open('yourmom.txt')
+
+# numLines = readFile.readlines()
+
+xAccelRaw = []
+xAccelCal = []
+datas = []
+time = []
+timediff = []
+velocity = []
+velocity.insert(0, 0)
+
+with open('April 15 Trial 2.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    for line in csv_reader:
+        datas.append(line)
+
+for x in range(len(datas)):
+    if datas[x][0] != '':
+        time.append(datas[x][0])
+        timediff.append(((int(datas[x][0])-(int(datas[0][0])-1))/1000))
+        xAccelRaw.append(datas[x][1])
+        cal = float(xAccelRaw[x])*(-1.085)-26.272
+        xAccelCal.append(cal)
+    else:
+        break
+
+xAccelCal.insert(0, 0)
+timediff.insert(0, 0)
+
+for x in range(1, len(xAccelCal)):
+    velocity.append(float(velocity[x-1]) + (float(timediff[x])-float(timediff[x-1]))*(float(xAccelCal[x])+ float(xAccelCal[x-1]))/2)
+
+for x in range(len(xAccelCal)):
+    print(str(timediff[x]) + '\t' + str(velocity[x]))
+csv_file.close()
